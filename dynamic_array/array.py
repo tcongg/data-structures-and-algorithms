99ad959e31abbc025d99e3c49b779eb93e5db463
tcongg <tcongg@gmail.com>
2019-02-14 17:07:13 +0700

    Fix TestCase_4

--- dynamic_array/array.py
+++ dynamic_array/array.py
@@ -118,11 +118,8 @@ class DynamicArray:
         """
         if self._size == self._capacity:
             new_capacity = self._capacity * 2
-        elif self._size > 0 and self._size <= self._capacity / 4 :
-            if int(self._capacity / 16) > 1:
-                new_capacity = int(self._capacity / 2)
-            else:
-                return
+        elif self._size > 0 and self._size <= self._capacity / 4 and self._capacity > 16 :
+            new_capacity = int(self._capacity / 2)
         else:
             return
 
