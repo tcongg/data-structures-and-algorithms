20b594dd6fb770b57c98cd340ef3e37642d3f0b2
tcongg <39913558+tcongg@users.noreply.github.com>
2019-02-17 20:43:16 +0700

    Add linked list implementation (#2)
    * Add linked list implementation
    
    * Fix declaration and assign value to the node
    
    * Rename variables and optimal code of functions
    
    * Rename variables and fix function remove_value(), TestCase
    
    * Fix function remove_value() and TestCase
    
    * Fix function remove_value()

---
+++ linked_list/linked_list.py
@@ -0,0 +1,207 @@
+class Node:
+
+    def __init__(self, data, next_node = None):
+        self.data = data
+        self.next = next_node
+
+class LinkedList:
+
+    def __init__(self):
+        self._head = None
+        self._size = 0
+
+    def size(self):
+        """
+        Returns number of data elements in list
+        Time Complexity: O(1)
+        """
+        return self._size
+
+    def is_empty(self):
+        """
+        Returns true if empty
+        Time Complexity: O(1)
+        """
+        return self._size == 0
+
+    def value_at(self, index):
+        """
+        Returns the value of the nth item (starting at 0 for first)
+        Time Complexity: O(n)
+        """
+        if index >= self._size or index < 0:
+            raise IndexError("Index out of range")
+
+        moved_node = self._head
+
+        for i in range(self._size):
+            if i == index:
+                return moved_node.data
+            moved_node = moved_node.next
+
+    def push_front(self, value):
+        """
+        Adds an item to the front of the list
+        Time Complexity: O(1)
+        """
+        self._head = Node(value, self._head)
+        self._size += 1
+
+    def pop_front(self):
+        """
+        Remove front item and return its value
+        Time Complexity: O(1)
+        """
+        if self._size == 0:
+            raise LookupError("Can not pop in empty linked list")
+
+        pop_value = self._head.data
+        self._head = self._head.next
+        self._size -= 1
+        return pop_value
+
+    def push_back(self, value):
+        """
+        Adds an item to the end of the list
+        Time Complexity: O(n)
+        """
+        if self._head is None:
+            self._head = Node(value)
+        else:
+            last_node = self._head
+
+            for i in range(self._size - 1):
+                last_node = last_node.next
+            last_node.next = Node(value)
+
+        self._size += 1
+
+    def pop_back(self):
+        """
+        Remove end item and return its value
+        Time Complexity: O(n)
+        """
+        if self._size == 0:
+            raise LookupError("Can not pop in empty linked list")
+
+        if self._size == 1:
+            pop_value = self._head.data
+            self._head = None
+        else:
+            moved_node = self._head
+
+            for i in range(self._size - 2):
+                moved_node = moved_node.next
+            pop_value = moved_node.next.data
+
+        self._size -= 1
+        return pop_value
+
+    def front(self):
+        """
+        Get value of front item
+        Time Complexity: O(1)
+        """
+        if self.is_empty():
+            return None
+
+        return self._head.data
+
+    def back(self):
+        """
+        Get value of end item
+        Time Complexity: O(n)
+        """
+        if self.is_empty():
+            return None
+
+        return self.value_at(self._size - 1)
+
+    def insert(self, index, value):
+        """
+        Insert value at index, so current item at that index is pointed to by new item at index
+        Time Complexity: O(n)
+        """
+        if index < 0 or index > self._size:
+            raise IndexError("Index out of range")
+
+        if index == 0:
+            self._head = Node(value, self._head)
+        else:
+            moved_node = self._head
+
+            for i in range(index - 1):
+                moved_node = moved_node.next
+
+            moved_node.next = Node(value, moved_node.next)
+
+        self._size += 1
+
+    def erase(self, index):
+        """
+        Removes node at given index
+        Time Complexity: O(n)
+        """
+        if index < 0 or index >= self._size:
+            raise IndexError("Index out of range")
+
+        if index == 0:
+            self._head = self._head.next
+        else:
+            moved_node = self._head
+
+            for i in range(index - 1):
+                moved_node = moved_node.next
+            moved_node.next = moved_node.next.next
+
+        self._size -= 1
+
+    def value_n_from_end(self, n):
+        """
+        Returns the value of the node at nth position from the end of the list
+        Time Complexity: O(n)
+        """
+        if n < 0 or n >= self._size:
+            raise IndexError("Index of value out of range")
+
+        return self.value_at(self._size - n - 1)
+
+    def reverse(self):
+        """
+        Reverse the list
+        Time Complexity: O(n)
+        """
+        temp_node = self._head
+        prev_node = None
+
+        for i in range(self._size):
+            next_node = temp_node.next
+            temp_node.next = prev_node
+            prev_node = temp_node
+            temp_node = next_node
+        self._head = prev_node
+
+    def remove_value(self, value):
+        """
+        Removes the first item in the list with this value
+        Time Complexity: O(n)
+        """
+        if self.is_empty():
+            return
+
+        if self._head.data == value:
+            self._head = self._head.next
+        else:
+            moved_node = self._head
+
+            while moved_node.next is not None:
+                if moved_node.next.data == value:
+                    moved_node.next = moved_node.next.next
+                    break
+                    
+                moved_node = moved_node.next
+
+            if moved_node.next is None:
+                return   
+
+        self._size -= 1       
---
+++ test/test_linked_list.py
@@ -0,0 +1,216 @@
+from unittest import TestCase
+from linked_list import LinkedList
+
+class TestLinkedList(TestCase):
+
+    def test_size(self):
+        linked_list = LinkedList()
+
+        self.assertEqual(linked_list.size(), 0)
+
+        linked_list.push_back(1)
+        self.assertEqual(linked_list.size(), 1)
+
+    def test_is_empty(self):
+        linked_list = LinkedList()
+
+        self.assertTrue(linked_list.is_empty())
+
+        linked_list.push_back(1)
+        self.assertFalse(linked_list.is_empty())
+
+    def test_value_at(self):
+        linked_list = LinkedList()
+
+        with self.assertRaises(IndexError) as context:
+            linked_list.value_at(1)
+        self.assertTrue("Index out of range" in str(context.exception))
+
+        linked_list.push_back(1)
+        linked_list.push_back(2)
+        linked_list.push_back(3)
+        self.assertEqual(linked_list.value_at(0), 1)
+        self.assertEqual(linked_list.value_at(1), 2)
+        self.assertEqual(linked_list.value_at(2), 3)
+
+    def test_push_front(self):
+        linked_list = LinkedList()
+
+        linked_list.push_front(1)
+        linked_list.push_front(2)
+        linked_list.push_front(3)
+        self.assertEqual(linked_list.value_at(0), 3)
+        self.assertEqual(linked_list.value_at(1), 2)
+        self.assertEqual(linked_list.value_at(2), 1)
+
+    def test_pop_front(self):
+        linked_list = LinkedList()
+
+        with self.assertRaises(LookupError) as context:
+            linked_list.pop_front()
+        self.assertTrue("Can not pop in empty linked list" in str(context.exception))
+
+        linked_list.push_front(1)
+        linked_list.push_front(2)
+        linked_list.push_front(3)
+        self.assertEqual(linked_list.pop_front(), 3)
+
+    def test_push_back(self):
+        linked_list = LinkedList()
+
+        linked_list.push_back(1)
+        linked_list.push_back(2)
+        linked_list.push_back(3)
+        self.assertEqual(linked_list.value_at(0), 1)
+        self.assertEqual(linked_list.value_at(1), 2)
+        self.assertEqual(linked_list.value_at(2), 3)
+
+    def test_pop_back(self):
+        linked_list = LinkedList()
+
+        with self.assertRaises(LookupError) as context:
+            linked_list.pop_back()
+        self.assertTrue("Can not pop in empty linked list" in str(context.exception))
+
+        linked_list.push_back(1)
+        linked_list.push_back(2)
+        linked_list.push_back(3)
+        self.assertEqual(linked_list.pop_back(), 3)
+
+    def test_front(self):
+        linked_list = LinkedList()
+
+        self.assertEqual(linked_list.front(), None)
+
+        linked_list.push_back(1)
+        linked_list.push_back(2)
+        linked_list.push_back(3)
+        self.assertEqual(linked_list.front(), 1)
+
+    def test_back(self):
+        linked_list = LinkedList()
+
+        self.assertEqual(linked_list.back(), None)
+
+        linked_list.push_back(1)
+        linked_list.push_back(2)
+        linked_list.push_back(3)
+        self.assertEqual(linked_list.back(), 3)
+
+    def test_insert(self):
+        linked_list = LinkedList()
+
+        with self.assertRaises(IndexError) as context:
+            linked_list.insert(1, 100)
+            linked_list.insert(-1, 200)
+        self.assertTrue("Index out of range" in str(context.exception))
+
+        linked_list.push_back(1)
+        linked_list.push_back(2)
+        linked_list.push_back(3)
+
+        linked_list.insert(0, 99)
+        linked_list.insert(4, 88)
+        linked_list.insert(2, 55)
+        self.assertEqual(linked_list.value_at(0), 99)
+        self.assertEqual(linked_list.value_at(1), 1)
+        self.assertEqual(linked_list.value_at(2), 55)
+        self.assertEqual(linked_list.value_at(3), 2)
+        self.assertEqual(linked_list.value_at(4), 3)
+        self.assertEqual(linked_list.value_at(5), 88)
+
+    def test_erase(self):
+        linked_list = LinkedList()
+
+        with self.assertRaises(IndexError) as context:
+            linked_list.erase(1)
+            linked_list.erase(-1)
+        self.assertTrue("Index out of range" in str(context.exception))
+
+        linked_list.push_back(1)
+        linked_list.push_back(2)
+        linked_list.push_back(3)
+        linked_list.push_back(4)
+        linked_list.push_back(5)
+
+        linked_list.erase(0)
+        linked_list.erase(3)
+        linked_list.erase(1)
+        self.assertEqual(linked_list.value_at(0), 2)
+        self.assertEqual(linked_list.value_at(1), 4)
+
+    def test_value_n_from_end(self):
+        linked_list = LinkedList()
+
+        with self.assertRaises(IndexError) as context:
+            linked_list.value_n_from_end(1)
+            linked_list.value_n_from_end(-1)
+        self.assertTrue("Index of value out of range" in str(context.exception))
+
+        linked_list.push_back(1)
+        linked_list.push_back(2)
+        linked_list.push_back(3)
+        linked_list.push_back(4)
+
+        self.assertEqual(linked_list.value_n_from_end(0), 4)
+        self.assertEqual(linked_list.value_n_from_end(2), 2)
+        self.assertEqual(linked_list.value_n_from_end(3), 1)
+
+    def test_reverse(self):
+        linked_list = LinkedList()
+
+        linked_list.push_back(1)
+        linked_list.push_back(2)
+        linked_list.push_back(3)
+        linked_list.push_back(4)
+        self.assertEqual(linked_list.value_at(0), 1)
+        self.assertEqual(linked_list.value_at(1), 2)
+        self.assertEqual(linked_list.value_at(2), 3)
+        self.assertEqual(linked_list.value_at(3), 4)
+
+        linked_list.reverse()
+        self.assertEqual(linked_list.value_at(0), 4)
+        self.assertEqual(linked_list.value_at(1), 3)
+        self.assertEqual(linked_list.value_at(2), 2)
+        self.assertEqual(linked_list.value_at(3), 1)
+
+    def test_remove_value(self):
+        linked_list = LinkedList()
+
+        linked_list.push_back(1)
+        linked_list.push_back(2)
+        linked_list.push_back(3)
+        linked_list.push_back(4)
+        linked_list.push_back(3)
+        linked_list.push_back(3)
+        linked_list.push_back(6)
+
+        linked_list.remove_value(10)
+        self.assertEqual(linked_list.value_at(0), 1)
+        self.assertEqual(linked_list.value_at(1), 2)
+        self.assertEqual(linked_list.value_at(2), 3)
+        self.assertEqual(linked_list.value_at(3), 4)
+        self.assertEqual(linked_list.value_at(4), 3)
+        self.assertEqual(linked_list.value_at(5), 3)
+        self.assertEqual(linked_list.value_at(6), 6)
+
+        linked_list.remove_value(1)
+        self.assertEqual(linked_list.value_at(0), 2)
+        self.assertEqual(linked_list.value_at(1), 3)
+        self.assertEqual(linked_list.value_at(2), 4)
+        self.assertEqual(linked_list.value_at(3), 3)
+        self.assertEqual(linked_list.value_at(4), 3)
+        self.assertEqual(linked_list.value_at(5), 6)
+
+        linked_list.remove_value(6)
+        self.assertEqual(linked_list.value_at(0), 2)
+        self.assertEqual(linked_list.value_at(1), 3)
+        self.assertEqual(linked_list.value_at(2), 4)
+        self.assertEqual(linked_list.value_at(3), 3)
+        self.assertEqual(linked_list.value_at(4), 3)
+
+        linked_list.remove_value(3)
+        self.assertEqual(linked_list.value_at(0), 2)
+        self.assertEqual(linked_list.value_at(1), 4)
+        self.assertEqual(linked_list.value_at(2), 3)
+        self.assertEqual(linked_list.value_at(3), 3)
