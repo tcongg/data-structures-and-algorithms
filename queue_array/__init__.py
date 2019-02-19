42993e897e63494afd978e5c7995bb188095854b
tcongg <tcongg@gmail.com>
2019-02-19 16:38:45 +0700

    Add queue array implementation

---
+++ queue_array/__init__.py
@@ -0,0 +1,1 @@
+from queue_array.queue_array import Queue
---
+++ queue_array/queue_array.py
@@ -0,0 +1,45 @@
+class Queue:
+
+    def __init__(self, capacity):
+        self._capacity = capacity
+        self._items = [None] * self._capacity
+        self._head = 0
+        self._tail = 0
+
+    def is_empty(self):
+        """
+        Checks array is empty or not
+        Time complexity: O(1)
+        """
+        return self._head == self._tail
+
+    def is_full(self):
+        """
+        Checks array is full or not
+        Time complexity: O(1)
+        """
+        return self._head == (self._tail + 1) % self._capacity
+
+    def enqueue(self, value):
+        """
+        Adds item at end of available storage
+        Time complexity: O(1)
+        """
+        if self.is_full():
+            return
+
+        self._items[self._tail] = value
+        self._tail = (self._tail + 1) % self._capacity
+
+    def dequeue(self):
+        """
+        Returns value and removes least recently added element
+        Time complexity: O(1)
+        """
+        if self.is_empty():
+            raise LookupError("Queue is empty")
+
+        dequeue_value = self._items[self._head]
+        self._items[self._head] = None
+        self._head = (self._head + 1) % self._capacity
+        return dequeue_value
