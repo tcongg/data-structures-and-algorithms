a142bee95232c7ff73d9225dc38d363bdd81406a
tcongg <tcongg@gmail.com>
2019-03-08 15:30:46 +0700

    Fix methods add(), get(), remove(), __resize()

--- hash_table_open_addressing/hash_table_open_addressing.py
+++ hash_table_open_addressing/hash_table_open_addressing.py
@@ -6,9 +6,9 @@ class Value():
 
 class HashTable():
 
-    def __init__(self, capacity):
+    def __init__(self):
         self._size = 0
-        self._capacity = capacity
+        self._capacity = 4
         self._table = [None] * self._capacity
 
     def __hash(self, key, i):
@@ -20,52 +20,61 @@ class HashTable():
 
     def add(self, key, value):
         self.__resize()
-
         for i in range(self._capacity):
             index = self.__hash(str(key), i)
-            if self._table[index] is None:
+            if self._table[index] is None or self._table[index].value is "Flag":
                 self._table[index] = Value(key, value)
                 self._size += 1
                 return
 
     def get(self, key):
-        index = self.__hash(str(key), 0)
+        for i in range(self._capacity):
+            index = self.__hash(str(key), i)
 
-        if self.exists(key):
-            return self._table[index].value
+            if self._table[index] is not None and self._table[index].key == key:
+                return self._table[index].value
 
         return None
 
     def exists(self, key):     
-        index = self.__hash(str(key), 0)
-        if self._table[index] == None:
-            return False
+        for i in range(self._capacity):
+            index = self.__hash(str(key), i)
+            if self._table[index] == None:
+                return False
 
-        if self._table[index].key == key:
-            return True
+            if self._table[index].key == key:
+                return True
 
         return False
 
     def remove(self, key):
-        index = self.__hash(str(key), 0)
-
-        if self.exists(key) is False:
-            return
-        
-        self._table[index] = Value(None, None)
+        for i in range(self._capacity):
+            index = self.__hash(str(key), i)
+            if self._table[index] != None and self._table[index].key == key:
+                self._table[index] = Value(None, "Flag")
+                self._size -= 1
+                self.__resize()
+                return
 
     def __resize(self):
         if self._size == self._capacity:
             new_capacity = self._capacity * 2
+        elif self._size > 0 and self._size <= self._capacity / 4 and self._capacity > 4:
+            new_capacity = self._capacity // 2
         else:
             return
+        
+        self._capacity = new_capacity
+        new_table = [None] * self._capacity
 
-        new_table = [None] * new_capacity
-
-        for i in range(self._capacity):
-            new_table[i] = self._table[i]
+        for item in self._table:
+            if item is not None and item.value != "Flag":
+                for i in range(self._capacity):
+                    new_index = self.__hash(str(item.key), i)
+                    if new_table[new_index] is None:
+                        new_table[new_index] = item
+                        break
 
         del self._table[:]
 
         self._table = new_table
-        self._capacity = new_capacity
--- test/test_hash_table_open_addressing.py
+++ test/test_hash_table_open_addressing.py
@@ -4,7 +4,7 @@ from hash_table_open_addressing import HashTable
 class TestHashTable(unittest.TestCase):
 
     def test_add(self):
-        table = HashTable(5)
+        table = HashTable()
 
         table.add(1, 1)
         self.assertEqual(table.get(1), 1)
@@ -15,20 +15,23 @@ class TestHashTable(unittest.TestCase):
         self.assertEqual(table.get(3), 3)
 
     def test_get(self):
-        table = HashTable(5)
+        table = HashTable()
 
         self.assertEqual(table.get(100), None)
 
-        table.add(1, 1)
-        self.assertEqual(table.get(1), 1)
+        table.add(7, 1)
+        self.assertEqual(table.get(7), 1)
 
-        table.add(2, 2)
-        table.add(3, 3)
-        self.assertEqual(table.get(2), 2)
-        self.assertEqual(table.get(3), 3)
+        table.add(16, 2)
+        table.add(25, 3)
+        self.assertEqual(table.get(16), 2)
+        self.assertEqual(table.get(25), 3)
+
+        table.remove(16)
+        self.assertEqual(table.get(25), 3)
 
     def test_exists(self):
-        table = HashTable(5)
+        table = HashTable()
 
         self.assertFalse(table.exists(8))
 
@@ -36,14 +39,12 @@ class TestHashTable(unittest.TestCase):
         self.assertTrue(1)
 
     def test_remove(self):
-        table = HashTable(5)
+        table = HashTable()
 
         table.add(1, 1)
         table.add(2, 2)
         table.add(3, 3)
         table.add(4, 4)
-
-        table.remove(6)
 
         table.remove(1)
         self.assertEqual(table.get(1), None)
